7d7cfde1e1992741a30df2921e530990be1cc472
rsi <root@kali-raspberry-pi>
2022-05-22 03:53:11 +0000

    update cfg

--- log_analysis.py
+++ log_analysis.py
@@ -31,7 +31,7 @@ with open(lao,'w') as f:
         cli,des = one_line[0],one_line[1]
         if cli.startswith('>'):
             cmd_exec(cli[1:])
-            print(cli[1:])
+            print('>>>',cli[1:])
             continue
         f.write(f'{des.strip()}   命令:{cli}\n')
         print('+++',cli)
